bb5bed72c0de7d6e63ff5d6c786d2898ee10d5a5
diviner <diviner.yang@cimforce.com>
2020-11-02 15:44:17 +0800

    Add ch3 course practice and fix in ch2 theMethod comment concept explain error

---
+++ java/code/ch3/ClassAndObj.java
@@ -0,0 +1,15 @@
+// class and object concept
+// A Class is like an object constructor, or a "blueprint" for creating objects.
+
+// create class
+public class ClassAndObj {
+    int x = 10;
+    public static void main(String[] args){
+        // create object
+        ClassAndObj MyObject = new ClassAndObj();
+        System.out.println(MyObject.x);
+    }
+}
+
+// Remember from the Java Syntax chapter that a class should always start with an uppercase first letter ,
+// and that the name of the java file should match the class name.
---
+++ java/code/ch3/README.md
@@ -0,0 +1,6 @@
+# 進階 java 概念與操作
+
+## OOP 前言
+撰寫程式是關於'寫下操作資料的一系列流程或方法'，而物件導向程式是關於'將資料與方法定義為物件並進行操作'
+### 參考資料
+[procedural programming vs object-oriented programming](https://www.geeksforgeeks.org/differences-between-procedural-and-object-oriented-programming/)
